2ecb3bb6cb7e7b9f074f7e717cbb9a116586753b
PB Github <phuong.buitan95@gmail.com>
2023-04-27 18:18:50 +0700

    Live Server extension

--- counter.js
+++ counter.js
@@ -2,6 +2,7 @@
 if (!localStorage.getItem('counter')) {
     localStorage.setItem('counter', 0);
 }
+
 function count(){
     let counter = localStorage.getItem('counter');
     counter++;
@@ -11,10 +12,20 @@ function count(){
     // if (counter % 10 === 0) {
     //     alert(`Count is now ${counter}`);
     // }
+    // return counter;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
+    let count_value = document.querySelector('h1').innerHTML;
+    alert(count_value*2)
+    
     // setInterval(count, 1000);
+    if (count_value % 2 == 1) {
+        document.querySelector('p').innerHTML = 'This is an odd number';
+    } 
+    else {
+        document.querySelector('p').innerHTML = 'This is an even number';
+    }
 });
